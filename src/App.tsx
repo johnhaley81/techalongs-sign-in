b142eeabc629c6ca9d29e7dfbe45dc83dbda50ea
John Haley <john@haley.io>
2024-11-17 19:10:33 -0700

    fix build warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import logo from './assets/logo.png'
+// import logo from './assets/logo.png'
 import './App.css'
 import {Autocomplete, AutocompleteItem, Button} from "@nextui-org/react";
 import {Switch} from "@nextui-org/react";
